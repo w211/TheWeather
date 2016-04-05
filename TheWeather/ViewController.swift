fe762ecdd24ee825d7732d0e74a9193841f5e7c1
Richard Cuico <rcuico@gmail.com>
2016-04-04 19:37:35 -0700

    Initial Commit

---
+++ TheWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TheWeather
+//
+//  Created by Marshall Cuico on 4/4/16.
+//  Copyright © 2016 Marshall Cuico. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

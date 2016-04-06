fbc50b9ee935d75e621e425c10d11609ed55a0de
Marshall Cuico <marshallcuico@Marshalls-MacBook-Pro.local>
2016-04-05 22:57:31 -0700

    Added TransparentNavigationBar

--- TheWeather/ViewController.swift
+++ TheWeather/ViewController.swift
@@ -12,14 +12,20 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), forBarMetrics: .Default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.translucent = true
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    override func viewDidAppear(animated: Bool) {
+        
+    }
 
 }
 
